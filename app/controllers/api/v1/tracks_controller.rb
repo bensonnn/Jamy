9e44d9233ac1284b1a0e00e4e5da36bb76ec9498
dannybenson <dan.s.benson@gmail.com>
2014-05-28 00:10:26 -0700

    refactor controller to include page param

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -1,16 +1,19 @@
 module Api
 	module V1
 		class TracksController < ApplicationController
-			# respond_to :json
 
 			def latest_blogged
-				render json: Track.latest_blogged(limit = 30, page = 1), root: false
+				render json: Track.latest_blogged(limit = 30, params["page"].to_i), root: false
 			end
 
 			def latest_released
-				render json: Track.latest_released(limit = 30, page = 1), root: false
+				render json: Track.latest_released(limit = 30, params["page"].to_i), root: false
 			end
 
+			def popular
+				render json: Track.popular(limit = 30, params["page"].to_i), root: false
+			end
+			
 		end
 	end
 end
