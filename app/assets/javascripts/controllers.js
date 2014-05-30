7b7c9431e36f46ab4b16a0e8b6f5cd4f601edc1e
dannybenson <dan.s.benson@gmail.com>
2014-05-30 14:19:38 -0700

    controller refactor with $scope

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,39 +1,36 @@
 (function() {
 	var app = angular.module('JamyApp.controllers', ['ngSanitize','JamyApp.services','ngTouch']);
 
-  app.controller('BloggedCtrl', ['$http','$route','player', function($http,$route, player) {
-    var latest = this;
-    this.player = player
-    this.page = 1
-    latest.name = "Latest<span>Blogged</span>"
+  app.controller('BloggedCtrl', ['$http','$route','player','$scope', function($http,$route,player,$scope) {
+    $scope.player = player
+    $scope.page = 1
+    $scope.name = "Latest<span>Blogged</span>"
     $http.get('/api/v1/latest_blogged.json', {
-      params: { page: latest.page }
+      params: { page: $scope.page }
     }).success(function(data){
-      latest.tracks = data
+      $scope.tracks = data
     });
   }])
 
-  app.controller('ReleasedCtrl', ['$http','$route','player', function($http,$route,player) {
-    var latest = this;
-    this.player = player
-    this.page = 1
-    this.name = "Latest<span>Released</span>"
+  app.controller('ReleasedCtrl', ['$http','$route','player','$scope', function($http,$route,player,$scope) {
+    $scope.player = player
+    $scope.page = 1
+    $scope.name = "Latest<span>Released</span>"
     $http.get('/api/v1/latest_released.json',  {
-      params: { page: latest.page }
+      params: { page: $scope.page }
     }).success(function(data){
-      latest.tracks = data
+      $scope.tracks = data
     });
   }])
 
-  app.controller('PopularCtrl', ['$http','$route','player', function($http,$route, player) {
-    var latest = this;
-    this.player = player
-    this.page = 1
-    this.name = "Popular"
+  app.controller('PopularCtrl', ['$http','$route','player','$scope', function($http,$route,player,$scope) {
+    $scope.player = player
+    $scope.page = 1
+    $scope.name = "Popular"
     $http.get('/api/v1/popular.json', {
-      params: { page: latest.page }
+      params: { page: $scope.page }
     }).success(function(data){
-      latest.tracks = data
+      $scope.tracks = data
     });
   }])
 
