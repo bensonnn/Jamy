1a214e0bea3b733c783c110d2d11d70487bb3686
dannybenson <dan.s.benson@gmail.com>
2014-05-30 22:40:18 -0700

    separate initializers file

---
+++ app/assets/javascripts/initializers.js
@@ -0,0 +1,9 @@
+(function() {
+  var app = angular.module('JamyApp.initializers', []);
+
+  SC.initialize({
+    client_id: "54124d08066b77ab0662dc6727e7bf39",
+    redirect_uri: "#",
+  });
+
+})();
