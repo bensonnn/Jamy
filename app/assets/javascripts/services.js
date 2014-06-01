0d94ad424bd695b1f4b3d8f43180f6ce339a4df9
dannybenson <dan.s.benson@gmail.com>
2014-06-01 15:06:57 -0700

    prev & next features

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,9 +1,10 @@
 (function() {
-	var app = angular.module('JamyApp.services', ['JamyApp.initializers','JamyApp.factories']);
+	var app = angular.module('JamyApp.services', ['JamyApp.controllers','JamyApp.initializers','JamyApp.factories']);
 
-  app.service('player', function($rootScope,trackObj) {
+  app.service('player', ['$rootScope','trackObj', function($rootScope,trackObj, ActiveTabCtrl) {
     var p = $rootScope;
     p.currentTrack = null;
+    p.tracks = []
 
     p.resume = function() {
       p.currentTrack.resume();
@@ -13,16 +14,27 @@
       p.currentTrack.pause();
     };
 
-    p.play = function(track) {
+    p.play = function(track,tracks) {
+      if (tracks) p.tracks = tracks
       if (p.currentTrack) p.currentTrack.pause();
       if (p.currentTrack && p.currentTrack.id == track.track_id) {
         p.currentTrack.resume();
       } else {
-        p.currentTrack = new trackObj(track)
+        p.currentTrack  = new trackObj(track)
         p.currentTrack.play();
       } 
     } 
 
-  });
+    p.next = function() {
+      idx = p.tracks.map(function(track) {return track.track_id}).indexOf(p.currentTrack.id)
+      p.play(p.tracks[idx + 1])      
+    }
+
+    p.prev = function() {
+      idx = p.tracks.map(function(track) {return track.track_id}).indexOf(p.currentTrack.id)
+      p.play(p.tracks[idx - 1])
+    }
+
+  }]);
 
 })();
