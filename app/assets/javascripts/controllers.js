77a497de71993fc9c67cb86c314d9cde13b563a4
dannybenson <dan.s.benson@gmail.com>
2014-06-01 15:05:10 -0700

    support page # in routes and controllers

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,33 +1,31 @@
 (function() {
 	var app = angular.module('JamyApp.controllers', ['ngSanitize','JamyApp.services','ngTouch']);
 
-  app.controller('BloggedCtrl', ['$http','$route','player','$scope', function($http,$route,player,$scope) {
-    $scope.page = 1
+  app.controller('BloggedCtrl', ['$http','$route','player','$scope','$rootScope','$routeParams', function($http,$route,player,$scope,$rootScope,$routeParams) {
     $scope.name = "Latest<span>Blogged</span>"
     $http.get('/api/v1/latest_blogged.json', {
-      params: { page: $scope.page }
+      params: { page: $routeParams.page || 1 }
     }).success(function(data){
       $scope.tracks = data
     });
   }])
 
-  app.controller('ReleasedCtrl', ['$http','$route','player','$scope', function($http,$route,player,$scope) {
-    $scope.page = 1
+  app.controller('ReleasedCtrl', ['$http','$route','player','$scope','$rootScope','$routeParams', function($http,$route,player,$scope,$rootScope,$routeParams) {
     $scope.name = "Latest<span>Released</span>"
     $http.get('/api/v1/latest_released.json',  {
-      params: { page: $scope.page }
+      params: { page: $routeParams.page || 1 }
     }).success(function(data){
       $scope.tracks = data
     });
   }])
 
-  app.controller('PopularCtrl', ['$http','$route','player','$scope', function($http,$route,player,$scope) {
-    $scope.page = 1
+  app.controller('PopularCtrl', ['$http','$route','player','$scope','$rootScope','$routeParams', function($http,$route,player,$scope,$rootScope,$routeParams) {
     $scope.name = "Popular"
     $http.get('/api/v1/popular.json', {
-      params: { page: $scope.page }
+      params: { page: $routeParams.page || 1 }
     }).success(function(data){
       $scope.tracks = data
+      $rootScope.tracks = data
     });
   }])
 
