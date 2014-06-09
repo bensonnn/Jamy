311939d2333049b1f560c6d0135453b9b7be3d30
dannybenson <dan.s.benson@gmail.com>
2014-06-08 19:43:55 -0700

    config assets

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Jamy::Application.routes.draw do
     end
   end
 
-  
-  # get '*path' => "static_pages#index"
+
+  get '*path' => "/"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
